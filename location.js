8458c7d574aa6e6f715282717a723e412e5cc127
dalenachau <missdalenachau@gmail.com>
2022-12-14 14:34:01 +0000

    complete location kata

---
+++ location.js
@@ -0,0 +1,35 @@
+/* 
+
+This function will be called with an array of objects, each object representing a student on the Northcoders bootcamp.
+Some of the students have a specified location property which is a string of the office they will be attending during
+their time on the course, but for some students that property is missing.
+Your function should return a *new array*, where all objects that were missing a location have been updated so that
+they now have a location property with a value of "remote". All other properties should remain as they were.
+This function must be a *pure function*, i.e. it should have no side effects, and your test suite should reflect this.
+
+updateRemoteStudents([
+  { name: 'Hypatia', age: 31, location: 'leeds' },
+  { name: 'Ramanujan', age: 22 },
+  { name: 'Tao', age: 47, location: 'manchester' }
+]);
+
+// should return
+
+[
+  { name: 'Hypatia', age: 31, location: 'leeds' },
+  { name: 'Ramanujan', age: 22, location: 'remote' },
+  { name: 'Tao', age: 47, location: 'manchester' }
+]; */
+
+function updateRemoteStudents(arr) {
+    let updated = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!arr[i].hasOwnProperty('location')) {
+            arr[i].location = 'remote'
+        }
+        updated.push(arr[i]);
+    }
+    return updated;
+}
+
+module.exports = updateRemoteStudents;
---
+++ location.test.js
@@ -0,0 +1,65 @@
+const updateRemoteStudents = require('./location')
+
+describe('updateRemoteStudents', () => {
+    test('returns an empty array when passed an empty array', () => {
+        expect(updateRemoteStudents([])).toEqual([]);
+    });
+    test('returns array when passed an array with one object and does not need updating', () => {
+        expect(updateRemoteStudents([{ name: 'Hypatia', age: 31, location: 'leeds' }]))
+        .toEqual([{ name: 'Hypatia', age: 31, location: 'leeds' }]);
+    });
+    test('returns updated array when passed an array with one object and does not have a location', () => {
+        expect(updateRemoteStudents([{ name: 'Ramanujan', age: 22 }]))
+        .toEqual([{ name: 'Ramanujan', age: 22 , location: 'remote'}]);
+    });
+    test('returns updated array when passed an array with mutiple objects, 1 location needs updating', () => {
+        expect(updateRemoteStudents([
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22 },
+            { name: 'Tao', age: 47, location: 'manchester' }]))
+        .toEqual([
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22, location: 'remote' },
+            { name: 'Tao', age: 47, location: 'manchester' }]);
+    });
+    test('returns updated array when passed an array with mutiple objects, which does not needs updating', () => {
+        expect(updateRemoteStudents([
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22, location: 'remote'},
+            { name: 'Tao', age: 47, location: 'manchester' }]))
+        .toEqual([
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22, location: 'remote' },
+            { name: 'Tao', age: 47, location: 'manchester' }]);
+    });
+    test('returns updated array when passed an array with mutiple objects, multiple locations needs updating', () => {
+        expect(updateRemoteStudents([
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22 },
+            { name: 'Tao', age: 47, location: 'manchester' },
+            { name: 'Dee', age: 23 }]))
+        .toEqual([
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22, location: 'remote' },
+            { name: 'Tao', age: 47, location: 'manchester' },
+            { name: 'Dee', age: 23, location: 'remote' }]);
+    });
+    test('returns a new array', () => {
+        const input = [
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22 },
+            { name: 'Tao', age: 47, location: 'manchester' }];
+        const returnVal = updateRemoteStudents(input);
+        expect(input).not.toBe(returnVal);
+    });
+    test('does not mutate original object', () => {
+        const input = [
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22 },
+            { name: 'Tao', age: 47, location: 'manchester' }];
+        expect(input).toEqual([
+            { name: 'Hypatia', age: 31, location: 'leeds' },
+            { name: 'Ramanujan', age: 22 },
+            { name: 'Tao', age: 47, location: 'manchester' }]);
+    });
+});
